bd97020a60ab4d4a0964e11d1f3f56f4e566b683
FACTS  LIMITED <admin@factsafrica.com>
2020-08-27 15:24:21 +0300

    Create a transactions class

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
+  final List<Transaction> transactions = [
+    Transaction('t1', 'Breakfast', 200.2, DateTime.now()),
+    Transaction('t2', 'Lunch', 100.8, DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,8 +24,12 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Card(
-              child: Text('Chart Card'),
+            Container(
+              width: double.infinity,
+              child: Card(
+                child: Text('Chart Card'),
+                elevation: 5,
+              ),
             ),
             Card(
               child: Text('Expenses List'),
