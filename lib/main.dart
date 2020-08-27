bcd3a3b41e65894d2feee499339bfebcc81d1bc9
FACTS  LIMITED <admin@factsafrica.com>
2020-08-27 14:50:40 +0300

    Gentlemen, Shall we?!

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'My Expenses',
+            style: TextStyle(color: Colors.yellow),
+          ),
+        ),
+        body: Center(
+          child: Text('Widget Play ground'),
+        ),
+      ),
+    );
+  }
+}
