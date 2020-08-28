c164fd9da5bb2dd45f2daac92ff6acb38aeea2e1
FACTS  LIMITED <admin@factsafrica.com>
2020-08-28 12:09:03 +0300

    display a modal on tap of the FAB

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-import './widgets/user_transaction.dart';
+import './widgets/new_transaction.dart';
+import './models/transaction.dart';
+import './widgets/transaction_list.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final List<Transaction> _userTransactions = [
+    Transaction('t1', 'Breakfast', 200.2, DateTime.now()),
+    Transaction('t2', 'Lunch', 100.8, DateTime.now())
+  ];
+
+  void _addTransaction(String txTitle, double txAmount) {
+    final newTransaction =
+        Transaction(Random().toString(), txTitle, txAmount, DateTime.now());
+    setState(() {
+      _userTransactions.add(newTransaction);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            child: NewTransaction(_addTransaction),
+            behavior: HitTestBehavior.opaque,
+            onTap: () {},
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +60,7 @@ class MyApp extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            UserTransactions(),
+            TransactionList(_userTransactions),
           ],
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
@@ -40,7 +70,7 @@ class MyApp extends StatelessWidget {
             color: Colors.black87,
           ),
           backgroundColor: Colors.yellowAccent,
-          onPressed: () {},
+          onPressed: () => _startAddNewTransaction(context),
         ),
       ),
     );
