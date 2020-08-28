2447c34c4f10a90d6e632de2bce3ce5aaad2233e
FACTS  LIMITED <admin@factsafrica.com>
2020-08-28 11:26:34 +0300

    add a list view

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -4,7 +4,6 @@ import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
-
   final List<Transaction> userTransactions;
 
   TransactionList(this.userTransactions);
@@ -13,51 +12,46 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 400,
-      child: SingleChildScrollView(
-              child: Column(
-          children: userTransactions
-              .map(
-                (tx) => Card(
-                    color: Colors.black87,
-                    child: Row(
-                      children: [
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          margin:
-                              EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                          decoration: BoxDecoration(
-                            border:
-                                Border.all(color: Colors.yellowAccent, width: 2),
-                          ),
-                          child: Text(
-                            'Ksh. ${tx.amount.toString()}',
-                            style: TextStyle(
-                                color: Colors.yellowAccent,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16),
-                          ),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              tx.name,
-                              style: TextStyle(
-                                  color: Colors.yellowAccent,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18),
-                            ),
-                            Text(
-                              DateFormat().format(tx.date),
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ],
-                        )
-                      ],
-                    )),
-              )
-              .toList(),
-        ),
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+              color: Colors.black87,
+              child: Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.yellowAccent, width: 2),
+                    ),
+                    child: Text(
+                      'Ksh. ${userTransactions[index].amount.toString()}',
+                      style: TextStyle(
+                          color: Colors.yellowAccent,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        userTransactions[index].name,
+                        style: TextStyle(
+                            color: Colors.yellowAccent,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                      Text(
+                        DateFormat().format(userTransactions[index].date),
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  )
+                ],
+              ));
+        },
+        itemCount: userTransactions.length,
       ),
     );
   }
