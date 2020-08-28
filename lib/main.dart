05c6ab083beaa4bb81a878426518d122f0445ec1
FACTS  LIMITED <admin@factsafrica.com>
2020-08-28 07:20:49 +0300

    clean up main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-
-import './transaction.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction('t1', 'Breakfast', 200.2, DateTime.now()),
-    Transaction('t2', 'Lunch', 100.8, DateTime.now())
-  ];
   
   final titleController = TextEditingController();
   final amountController = TextEditingController();
@@ -60,50 +53,6 @@ class MyApp extends StatelessWidget {
                 ),
               ),
             ),
-            Column(
-              children: transactions
-                  .map(
-                    (tx) => Card(
-                        color: Colors.black87,
-                        child: Row(
-                          children: [
-                            Container(
-                              padding: EdgeInsets.all(10),
-                              margin: EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 10),
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                    color: Colors.yellowAccent, width: 2),
-                              ),
-                              child: Text(
-                                'Ksh. ${tx.amount.toString()}',
-                                style: TextStyle(
-                                    color: Colors.yellowAccent,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16),
-                              ),
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  tx.name,
-                                  style: TextStyle(
-                                      color: Colors.yellowAccent,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18),
-                                ),
-                                Text(
-                                  DateFormat().format(tx.date),
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ],
-                            )
-                          ],
-                        )),
-                  )
-                  .toList(),
-            )
           ],
         ),
       ),
