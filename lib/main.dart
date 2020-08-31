95e2d85b692506822cf12d59d101433d0547ee88
FACTS  LIMITED <admin@factsafrica.com>
2020-08-31 16:14:03 +0300

    add image when transaction is not available

--- lib/main.dart
+++ lib/main.dart
@@ -14,65 +14,79 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Personal Expenses',
-      theme: ThemeData(primarySwatch: Colors.green, accentColor: Colors.amber),
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        accentColor: Colors.amber,
+        fontFamily: 'Quicksand',
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+                headline6: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+        ),
+      ),
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-final List<Transaction> _userTransactions = [
-    Transaction('t1', 'Breakfast', 200.2, DateTime.now()),
-    Transaction('t2', 'Lunch', 100.8, DateTime.now())
+  final List<Transaction> _userTransactions = [
+    // Transaction('t1', 'Breakfast', 200.2, DateTime.now()),
+    // Transaction('t2', 'Lunch', 100.8, DateTime.now())
   ];
 
   void _addTransaction(String txTitle, double txAmount) {
-    final newTransaction = Transaction(Random().toString(), txTitle, txAmount, DateTime.now() );
+    final newTransaction =
+        Transaction(Random().toString(), txTitle, txAmount, DateTime.now());
     setState(() {
       _userTransactions.add(newTransaction);
     });
   }
 
-  void _startAddNewTransaction(BuildContext ctx ) {
-    showModalBottomSheet(context: ctx, builder: (_) {
-      return NewTransaction(_addTransaction);
-    });
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return NewTransaction(_addTransaction);
+        });
   }
 
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Personal Expenses',
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Personal Expenses',
         ),
-        body: ListView(
-          children: [
-            Container(
-              width: double.infinity,
-              child: Card(
-                child: Text('Chart Card'),
-                elevation: 5,
-              ),
+      ),
+      body: ListView(
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('Chart Card'),
+              elevation: 5,
             ),
-            TransactionList(_userTransactions),
-          ],
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: FloatingActionButton(
-          child: Icon(
-            Icons.add,
           ),
-          onPressed: () => _startAddNewTransaction(context),
+          TransactionList(_userTransactions),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
         ),
-      );
+        onPressed: () => _startAddNewTransaction(context),
+      ),
+    );
   }
 }
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,45 +12,61 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 600,
-      child: ListView.builder(
-        itemBuilder: (ctx, index) {
-          return Card(
-              child: Row(
-            children: [
-              Container(
-                padding: EdgeInsets.all(10),
-                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColorDark,
-                  border: Border.all(
-                      width: 2, color: Theme.of(context).primaryColorDark),
+      child: userTransactions.isEmpty
+          ? Column(
+              children: [
+                SizedBox(
+                  height: 10,
                 ),
-                child: Text(
-                  'Ksh. ${userTransactions[index].amount.toStringAsFixed(2)}',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: Theme.of(context).accentColor),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    userTransactions[index].name,
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Theme.of(context).primaryColorDark),
-                  ),
-                  Text(
-                    DateFormat().format(userTransactions[index].date),
-                    style: TextStyle(color: Colors.blueGrey),
-                  ),
-                ],
-              )
-            ],
-          ));
-        },
-        itemCount: userTransactions.length,
-      ),
+                Container(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ))
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                    child: Row(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColorDark,
+                        border: Border.all(
+                            width: 2,
+                            color: Theme.of(context).primaryColorDark),
+                      ),
+                      child: Text(
+                        'Ksh. ${userTransactions[index].amount.toStringAsFixed(2)}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Theme.of(context).accentColor),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          userTransactions[index].name,
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        Text(
+                          DateFormat().format(userTransactions[index].date),
+                          style: TextStyle(color: Colors.blueGrey),
+                        ),
+                      ],
+                    )
+                  ],
+                ));
+              },
+              itemCount: userTransactions.length,
+            ),
     );
   }
 }
