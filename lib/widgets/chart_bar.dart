d199b8603652fd52450b6cb8686a9fe5f60b0d72
FACTS  LIMITED <admin@factsafrica.com>
2020-08-31 17:13:53 +0300

    create a bar for the chart

---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double spentAmount;
+  final double percentageOfTotalSpent;
+
+  ChartBar(this.label, this.spentAmount, this.percentageOfTotalSpent);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('Ksh. ${spentAmount.toStringAsFixed(0)}'),
+        SizedBox(height: 4),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey, width: 1.0),
+                    color: Color.fromRGBO(220, 220, 220, 1),
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+              FractionallySizedBox(
+                heightFactor: percentageOfTotalSpent,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColorDark,
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+              )
+            ],
+          ),
+        ),
+        SizedBox(height: 4),
+        Text(label)
+      ],
+    );
+  }
+}
