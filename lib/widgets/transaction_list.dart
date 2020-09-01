107408c2c3d333409671f6510c23941d170b965b
FACTS  LIMITED <admin@factsafrica.com>
2020-09-01 19:08:53 +0300

    render different widgets based on device size

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,21 +12,21 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return userTransactions.isEmpty
-        ? LayoutBuilder(builder: (ctx, constraints){
-          return Column(
-            children: [
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                  height: constraints.maxHeight * 0.6,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  ))
-            ],
-          );
-        })
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ))
+              ],
+            );
+          })
         : ListView.builder(
             itemBuilder: (ctx, index) {
               return Card(
@@ -47,12 +47,18 @@ class TransactionList extends StatelessWidget {
                   ),
                   subtitle: Text(
                       '${DateFormat().add_yMMMMd().format(userTransactions[index].date)}'),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    color: Theme.of(context).errorColor,
-                    onPressed: () =>
-                        _removeTransaction(userTransactions[index].id),
-                  ),
+                  trailing: MediaQuery.of(context).size.width > 360
+                      ? FlatButton.icon(
+                          textColor: Theme.of(context).errorColor,
+                          onPressed: ()=>_removeTransaction(userTransactions[index].id),
+                          icon: Icon(Icons.delete),
+                          label: Text('Delete'))
+                      : IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Theme.of(context).errorColor,
+                          onPressed: () =>
+                              _removeTransaction(userTransactions[index].id),
+                        ),
                 ),
               );
             },
